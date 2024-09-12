38b6f4d8f6aba13027a629d445025a824fd65dca
ashoordoushow <ashoor12323@gmail.com>
2024-09-12 13:25:04 -0500

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# photo = Photo.find_by(id: 1)
+# p photo
+
+# photo = Photo.new(name: "spring", width: 234, height: 434)
+# photo.save
+# p photo
+
+# photo = Photo.new(name: "Cold", width: 2342, height: 949)
+# photo.save
+
+# photo = Photo.find_by(id: 1)
+# p photo
+# photo = Photo.find_by(id: 2)
+# p photo
+# photo = Photo.find_by(id: 3)
+# p photo
+# photo = Photo.find_by(id: 4)
+# p photo
+# photo = Photo.find_by(id: 5)
+# p photo
+# photo = Photo.find_by(id: 6)
+
+# p photo
+
+photo = Photo.all
+p  photo
